2b55bd3735f0ca4e2228aa39722c6a0a5a453f9f
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-05-21 02:42:42 +0300

    add the new folder structuring

--- app/requests.py
+++ app/requests.py
@@ -1,14 +1,18 @@
 from app import app
-from .models import movie
+from .models import Movie
 import urllib.request,json
 
-Movie = movie.Movie
 
-# getting api key
-api_key = app.config['MOVIE_API_KEY']
 
-# getting the movie base url
-base_url = app.config['MOVIE_API_BASE_URL']
+# Getting api key
+api_key = None
+# Getting the movie base url
+base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['MOVIE_API_KEY']
+    base_url = app.config['MOVIE_API_BASE_URL']
 
 def get_movies(category):
     '''
